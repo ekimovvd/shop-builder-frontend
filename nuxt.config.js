082e6dde41a1b8177b905d67a35acf31a08681b6
Vadim Ekimov <ekimov_vd@mail.ru>
2023-04-13 00:43:46 +0300

    Инициализация проекта

---
+++ nuxt.config.js
@@ -0,0 +1,31 @@
+export default {
+  head: {
+    title: 'Конструктор интернет-магазина',
+    htmlAttrs: {
+      lang: 'ru'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [
+    '~assets/styles/main.scss',
+  ],
+
+  styleResources: {
+    scss: ['~assets/styles/*.scss']
+  },
+
+  components: true,
+
+  modules: [
+    '@nuxtjs/style-resources',
+  ],
+}
